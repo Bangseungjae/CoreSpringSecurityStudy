398a9afdbf8dd28c1ada7f82b7cb315cc6eb6748
Bangseungjae <tmdwo5825@naver.com>
2022-06-13 14:30:47 +0900

    ;;

--- src/main/java/io/security/CoreSpringSecurity/repository/UserRepository.java
+++ src/main/java/io/security/CoreSpringSecurity/repository/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<Account, Long> {
 
     public Account findByUsername(String username);
+    int countByUsername(String username);
 
     public Account findByUsernameAndPassword(String username, String password);
 }
